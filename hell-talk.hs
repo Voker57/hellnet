15a8da8641c51a360f003627b468aa22afc7376a
Voker57 <voker57@gmail.com>
2009-06-26 18:29:48 +0400

    Forgotten source

---
+++ hell-talk.hs
@@ -0,0 +1,32 @@
+import Data.Maybe
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import Hellnet
+import Hellnet.Network
+import Hellnet.Storage
+import System.Environment
+
+talkMeta = Meta "talk" "0"
+
+prettyPrintMessage :: String -> IO ()
+prettyPrintMessage s = do
+	putStrLn ""
+	putStrLn s
+	putStrLn "-------------"
+
+printLatestTalks :: Int -> IO ()
+printLatestTalks n = do
+	hs <- findHashesByMeta talkMeta
+	msgs <- mapM (findChunk Nothing) (reverse $ take n $ reverse hs)
+	mapM_ (prettyPrintMessage) $ map (BS8.unpack) $ catMaybes msgs
+
+main = do
+	args <- getArgs
+	if null args then
+		printLatestTalks 10
+		else do
+			putStrLn "Please input message:"
+			text <- BS.getContents
+			hash <- insertChunk Nothing (BS.unpack text)
+			addHashesToMeta talkMeta [hash]
+			putStrLn "Posted!"
