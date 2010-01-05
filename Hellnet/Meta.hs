cac89a5293c663c2ed0bb87ec77f0d11846782d0
Voker57 <voker57@gmail.com>
2010-01-05 18:55:38 +0300

    Quite some meta fixes

--- Hellnet/Meta.hs
+++ Hellnet/Meta.hs
@@ -1,10 +1,11 @@
-module Hellnet.Meta (Meta(..), byteStringToMeta) where
+module Hellnet.Meta (Meta(..), fromByteString) where
 
 import Codec.Crypto.RSA
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.UTF8 as BU
+import qualified Data.Map as Map
 import Data.Maybe
 import Debug.Trace
 import Hellnet
@@ -18,32 +19,38 @@ data Meta = Meta {
 	, metaName :: String -- ^ Meta name
 	, timestamp :: Integer -- ^ Updated timestamp
 	, contentURI :: HellnetURI -- ^ Meta content location
-	, signature :: Signature -- ^ Digital signature
+	, message :: Maybe BS.ByteString -- ^ JSON as ByteString
+	, signature :: Maybe Signature -- ^ Digital signature
 	} deriving (Eq, Show)
 
-byteStringToMeta :: BS.ByteString -> Maybe Meta
-byteStringToMeta bs = let
+instance Jsonable Meta where
+	fromJson v = let
+		keyIDV = jPath' "key" v;
+		timestampV = jPath' "updated" v;
+		nameV = jPath' "name" v;
+		contentV = jPath' "content" v in
+			case [keyIDV, timestampV, nameV, contentV] of
+				[	[JString keyHash]
+					, [JNumber timestampF]
+					, [JString nameS]
+					, [JString contentURI]
+					] -> maybe (Nothing) (\u -> Just $ Meta {
+						keyID = hexToHash keyHash
+						, metaName = nameS
+						, timestamp = (round timestampF)
+						, contentURI = u
+						, signature = Nothing
+						, message = Nothing}) (parseHellnetURI contentURI)
+				otherwise -> Nothing
+	toJson m = toJson $ Map.fromList [
+		("key", toJson $ hashToHex $ keyID m)
+		, ("updated", toJson $ timestamp m)
+		, ("name", toJson $ metaName m)
+		, ("content", toJson $ show $ contentURI m)
+		]
+
+fromByteString :: BS.ByteString -> Maybe Meta
+fromByteString bs = let
 	(s, sig) = BS.breakSubstring (BS8.pack "\n\n") bs;
 	js = BU.toString s in
-		maybe (Nothing) (\v -> let
-			keyIdV = jPath' "key" v;
-			timestampV = jPath' "updated" v;
-			nameV = jPath' "name" v;
-			contentV = jPath' "content" v in
-				case [keyIDV, timestampV, nameV, contentV] of
-					[	[JString keyHash]
-						, [JNumber timestampF]
-						, [JString nameS]
-						, [JString contentURI]
-						] -> maybe (Nothing) (\u -> Just $ Meta {
-							keyID = hexToHash keyHash
-							, metaName = nameS
-							, timestamp = (round timestampF)
-							, contentURI = u
-							, signature = sig) (traceShow (parseHellnetURI contentURI) (parseHellnetURI contentURI))
-					otherwise -> Nothing) $ Json.fromString js
-
-verifyMeta :: BS.ByteString -> IO Bool
-verifyMeta bs = undefined
- 	let (dat, sig) = BS.breakSubstring (BS8.pack "\n\n") bs
- 	sigKey <- findChunk ()
+		either (const Nothing) (fromJson) $ Json.fromString js
